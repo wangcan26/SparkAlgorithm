fa6294c500ea9178c63ef9673b19bc9b53cc31ef
wangcan26 <>
2019-09-09 13:27:50 +0800

    first commit

---
+++ SparkDemo/SparkDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SparkDemo
+//
+//  Created by wangcan on 2019/9/8.
+//  Copyright © 2019 wangcan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ spark_algorithm.podspec
@@ -0,0 +1,9 @@
+Pod::Spec.new do |s|
+    s.name      = "SparkAlgorithm"
+    s.version   = "0.0.1"
+    s.summary   = "SparkAlgorithm for learn "
+    s.author    = {"can wang" => "tianxiayuxin@gmail.com"}
+    s.license   = "MIT"
+    s.source    = {}
+
+end
